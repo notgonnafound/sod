07479ba5846aa52a1319bdc7c45a2dac651ecf50
Glitch (hello-express) <none>
2021-02-25 20:06:20 +0000

    🏡🍶 Checkpoint ./server.js:29068751/4070 ./config.json:29068751/150

--- server.js
+++ server.js
@@ -1,34 +1,105 @@
-// server.js
-// where your node app starts
+const Discord = require('discord.js');
 
-// we've started you off with Express (https://expressjs.com/)
-// but feel free to use whatever libraries or frameworks you'd like through `package.json`.
-const express = require("express");
-const app = express();
+const client = new Discord.Client();
 
-// our default array of dreams
-const dreams = [
-  "Find and count some sheep",
-  "Climb a really tall mountain",
-  "Wash the dishes"
-];
+const { token, default_prefix } = require('./config.json');
 
-// make all the files in 'public' available
-// https://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+const { readdirSync } = require('fs');
 
-// https://expressjs.com/en/starter/basic-routing.html
-app.get("/", (request, response) => {
-  response.sendFile(__dirname + "/views/index.html");
+const { join } = require('path');
+
+const config = require('./config.json');
+client.config = config;
+
+const db = require('quick.db');
+
+
+
+const { GiveawaysManager } = require('discord-giveaways');
+
+client.giveawaysManager = new GiveawaysManager(client, {
+    storage: "./giveaways.json",
+    updateCountdownEvery: 5000,
+    default: {
+        botsCanWin: false,
+        exemptPermissions: [],
+        embedColor: "#FF0000",
+        reaction: "🎉"
+    }
 });
 
-// send the default array of dreams to the webpage
-app.get("/dreams", (request, response) => {
-  // express helps us take JS objects and send them as JSON
-  response.json(dreams);
+client.commands = new Discord.Collection();
+//You can change the prefix if you like. It doesn't have to be ! or ;
+const commandFiles = readdirSync(join(__dirname, "commands")).filter(file => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(join(__dirname, "commands", `${file}`));
+    client.commands.set(command.name, command);
+}
+
+
+client.on("error", console.error);
+
+client.on('ready', () => {
+    console.log('I am ready');
+    client.user.setActivity(`If I'm online, you might be recorded`)
 });
 
-// listen for requests :)
-const listener = app.listen(process.env.PORT, () => {
-  console.log("Your app is listening on port " + listener.address().port);
-});
+
+let stats = {
+    serverID: '<ID>',
+    total: "<ID>",
+    member: "<ID>",
+    bots: "<ID>"
+}
+
+
+
+client.on('guildMemberAdd', member => {
+    if (member.guild.id !== stats.serverID) return;
+    client.channels.cache.get(stats.total).setName(`Total Users: ${member.guild.memberCount}`);
+    client.channels.cache.get(stats.member).setName(`Members: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
+    client.channels.cache.get(stats.bots).setName(`Bots: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
+})
+
+client.on('guildMemberRemove', member => {
+    if (member.guild.id !== stats.serverID) return;
+    client.channels.cache.get(stats.total).setName(`Total Users: ${member.guild.memberCount}`);
+    client.channels.cache.get(stats.member).setName(`Members: ${member.guild.members.cache.filter(m => !m.user.bot).size}`);
+    client.channels.cache.get(stats.bots).setName(`Bots: ${member.guild.members.cache.filter(m => m.user.bot).size}`);
+
+
+})
+
+client.on("message", async message => {
+
+    if (message.author.bot) return;
+    if (message.channel.type === 'dm') return;
+
+    let prefix = await db.get(`prefix_${message.guild.id}`);
+    if (prefix === null) prefix = default_prefix;
+
+    const escapeRegex = str => str.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+    const prefixRegex = new RegExp(`^(<@!?${client.user.id}>|${escapeRegex(prefix)})\\s*`);
+
+    if(!prefixRegex.test(message.content)) return;
+
+    const [, matchedPrefix] = message.content.match(prefixRegex);
+
+
+        const args = message.content.slice(matchedPrefix.length).trim().split(/ +/g);
+
+        const command = args.shift().toLowerCase();
+
+        if (!client.commands.has(command)) return;
+
+
+        try {
+            client.commands.get(command).run(client, message, args);
+
+        } catch (error) {
+            console.error(error);
+        }
+})
+
+client.login(token);
