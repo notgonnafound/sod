82fedc54f2da6a9d64b87466936152d10a04e8e0
Glitch (hello-express) <none>
2021-02-25 20:52:14 +0000

    🐝🔦 Checkpoint ./commands/help.js:29068751/587

--- commands/help.js
+++ commands/help.js
@@ -6,10 +6,14 @@ module.exports = {
 
         const reason = args.join(" ");
 
-       let staffrole = message.guild.roles.find("name","staff")
-       let voice = message.author
+       let staffrole = message.guild.roles.cache.find(role => role.name === "staff");
+       let mem = message.author
        
+       if (mem.voice.channel) {
+       let voice = mem.voice.channel.name
+       message.channel.send(`${staffrole} | ${message.author.tag} need your help \n voice channel: ${voice} \n reason: ${reason}`)
+       }else{
        message.channel.send(`${staffrole} | ${message.author.tag} need your help \n `)
-
+       }
     }
 }
