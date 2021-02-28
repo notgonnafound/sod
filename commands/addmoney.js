d9d373e501ad5386b2335341f9ec46c25fa0cbcb
Glitch (hello-express) <none>
2021-02-28 03:54:43 +0000

    🙋⚓️ Checkpoint ./commands/shop.js:29068751/166 ./commands/work.js:29068751/878 ./commands/addmoney.js:29068751/1681

---
+++ commands/addmoney.js
@@ -0,0 +1,21 @@
+const db = require('quick.db');
+const ms = require('parse-ms');
+
+module.exports = {
+    name: "addmoney",
+    description: "Work your a** off",
+
+    async run (client, message, args) {
+        
+        let user = message.mentions.members.first() || message.guild.members.cache.get(args[0]);
+        
+      
+
+  
+            let amount = args[1]; 
+            db.add(`money_${message.guild.id}_${user.id}`, amount)
+            let bal = await db.fetch(`money_${message.guild.id}_${user.id}`);
+            message.channel.send(`${user}, you got ${amount} coins \n now you have ${bal} coins`)
+        
+    }
+}
--- commands/shop.js
+++ commands/shop.js
@@ -7,11 +7,12 @@ module.exports = {
     async run (client, message, args) {
   
       
-        let r1 = message.guild.roles.cache.find(x => x.name === "Muted");
+        let r1 = message.guild.roles.cache.find(x => x.id === "815428440433360899");
+        let r2 = message.guild.roles.cache.find(x => x.id === "815428498264031263");
 
         const embed = new Discord.MessageEmbed()
         .setTitle('Store')
-        .setDescription(`1 - 🌈 - 500 coins \n 2 - 🌬 - 250 coins`)
+        .setDescription(`1 - ${r1} - 500 coins \n 2 - ${r2} - 250 coins`)
         .setTimestamp();
 
         message.channel.send(embed);
