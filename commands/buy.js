342ba29fd36834dbb1696b9094883cc859e6e8a8
Glitch (hello-express) <none>
2021-02-28 04:52:03 +0000

    🎡👏 Checkpoint ./commands/buy.js:29068751/1171 ./commands/inv.js:29068751/2

--- commands/buy.js
+++ commands/buy.js
@@ -14,7 +14,16 @@ module.exports = {
         let r2 = message.guild.roles.cache.find(x => x.id === "815428498264031263");
         const user = message.author;
         if(purchase === '1'){
-            if (db.has(message.author.id, { items: ["r3"] })){
+           // if (db.has(message.author.id, { items: ["r3"] }))
+          let items = await db.fetch(message.author.id);
+          if (items === null){
+            if(amount < 500) return message.channel.send('You do not have enough money to buy this role. Please try another one');
+            message.member.roles.add(r1);
+            db.subtract(`money_${message.guild.id}_${message.author.id}`, 500);
+            db.push(message.author.id, `${r1}`);
+            return message.channel.send(`Successfully bought the role ${r1}`)
+          }
+          if (items.includes(`${r1}`)){
                 return message.reply("Already got a that role bro!")
 
 } else {
@@ -25,17 +34,6 @@ module.exports = {
             message.channel.send(`Successfully bought the role ${r1}`)
         }
     }
-        if(purchase === '2'){
-            if (db.has(message.author.id, { items: ["r2"] })){
-                return message.reply("Already got a that role bro!")
 
-} else {
-            if(amount < 250) return message.channel.send('You do not have enough money to buy this role. Please try another one');
-            message.member.roles.add(r2);
-            db.subtract(`money_${message.guild.id}_${message.author.id}`, 250);
-            db.push(message.author.id, `${r2}`);
-            message.channel.send(`Successfully bought the role ${r2}`)
-        }
-    }
     }
 }
