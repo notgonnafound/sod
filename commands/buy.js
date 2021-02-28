95211411ef14720203995bd9146e649438dcdf17
Glitch (hello-express) <none>
2021-02-28 04:29:40 +0000

    🚍☕️ Checkpoint ./commands/inv.js:29068751/441 ./commands/buy.js:29068751/196

--- commands/buy.js
+++ commands/buy.js
@@ -14,14 +14,14 @@ module.exports = {
         let r2 = message.guild.roles.cache.find(x => x.id === "815428498264031263");
         const user = message.author;
         if(purchase === '1'){
-            if (db.has(`variable`, { items: ["r1"] })){
+            if (db.has(`variable`,{ items: [] })){
                 return message.reply("Already got a that role bro!")
 
 } else {
             if(amount < 500) return message.channel.send('You do not have enough money to buy this role. Please try another one');
             message.member.roles.add(r1);
             db.subtract(`money_${message.guild.id}_${message.author.id}`, 500);
-            db.push(message.author.id, "r1");
+            db.push(message.author.id, `${r1}`);
             message.channel.send(`Successfully bought the role ${r1}`)
         }
     }
