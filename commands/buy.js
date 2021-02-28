e05bec8967fa410445833341e3071f9bd2353106
Glitch (hello-express) <none>
2021-02-28 04:18:01 +0000

    💓🛣 Checkpoint ./commands/buy.js:29068751/134

--- commands/buy.js
+++ commands/buy.js
@@ -12,13 +12,14 @@ module.exports = {
         let amount = await db.fetch(`money_${message.guild.id}_${message.author.id}`)
         let r1 = message.guild.roles.cache.find(x => x.id === "815428440433360899");
         let r2 = message.guild.roles.cache.find(x => x.id === "815428498264031263");
+        const user = message.author;
         if(purchase === '1'){
             if (db.has(`variable`, { items: ["r1"] })){
                 return message.reply("Already got a that role bro!")
 
 } else {
             if(amount < 500) return message.channel.send('You do not have enough money to buy this role. Please try another one');
-            user.roles.add(role);
+            message.member.roles.add(r1);
             db.subtract(`money_${message.guild.id}_${message.author.id}`, 500);
             db.push(message.author.id, "r1");
             message.channel.send(`Successfully bought the role ${r1}`)
@@ -30,6 +31,7 @@ module.exports = {
 
 } else {
             if(amount < 500) return message.channel.send('You do not have enough money to buy this role. Please try another one');
+            user.roles.add(r2);
             db.subtract(`money_${message.guild.id}_${message.author.id}`, 500);
             db.push(message.author.id, "r2");
             message.channel.send(`Successfully bought the role ${r2}`)
