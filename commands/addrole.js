686e4c7c4300dc0198a49e59f828880d03e7dc44
Glitch (honey-lava-orca) <none>
2021-03-02 10:43:21 +0000

    📇🎻 Checkpoint ./commands/addrole.js:29068751/70

--- commands/addrole.js
+++ commands/addrole.js
@@ -14,61 +14,61 @@ module.exports = {
         db.set(`r1_${message.guild.id}`, args[1])
         db.set(`r1_price_${message.guild.id}`, args[2])
         let role = message.guild.roles.cache.find(x => x.id === args[1]);
-        message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
+        return message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
       }
       if(args[0] == "2") {
         db.set(`r2_${message.guild.id}`, args[1])
         db.set(`r2_price_${message.guild.id}`, args[2])
         let role = message.guild.roles.cache.find(x => x.id === args[1]);
-        message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
+        return message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
       }     
       if(args[0] == "3") {
         db.set(`r3_${message.guild.id}`, args[1])
         db.set(`r3_price_${message.guild.id}`, args[2])
         let role = message.guild.roles.cache.find(x => x.id === args[1]);
-        message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
+        return message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
       } 
         if(args[0] == "4") {
         db.set(`r4_${message.guild.id}`, args[1])
         db.set(`r4_price_${message.guild.id}`, args[2])
         let role = message.guild.roles.cache.find(x => x.id === args[1]);
-        message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
+        return message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
       }
       if(args[0] == "5") {
         db.set(`r5_${message.guild.id}`, args[1])
         db.set(`r5_price_${message.guild.id}`, args[2])
         let role = message.guild.roles.cache.find(x => x.id === args[1]);
-        message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
+        return message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
       }
       if(args[0] == "6") {
         db.set(`r6_${message.guild.id}`, args[1])
         db.set(`r6_price_${message.guild.id}`, args[2])
         let role = message.guild.roles.cache.find(x => x.id === args[1]);
-        message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
+        return message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
       }
       if(args[0] == "7") {
         db.set(`r7_${message.guild.id}`, args[1])
         db.set(`r7_price_${message.guild.id}`, args[2])
         let role = message.guild.roles.cache.find(x => x.id === args[1]);
-        message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
+        return message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
       }
       if(args[0] == "8") {
         db.set(`r8_${message.guild.id}`, args[1])
         db.set(`r8_price_${message.guild.id}`, args[2])
         let role = message.guild.roles.cache.find(x => x.id === args[1]);
-        message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
+        return message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
       }
       if(args[0] == "9") {
         db.set(`r9_${message.guild.id}`, args[1])
         db.set(`r9_price_${message.guild.id}`, args[2])
         let role = message.guild.roles.cache.find(x => x.id === args[1]);
-        message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
+        return message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
       }
       if(args[0] == "10") {
         db.set(`r10_${message.guild.id}`, args[1])
         db.set(`r10_price_${message.guild.id}`, args[2])
         let role = message.guild.roles.cache.find(x => x.id === args[1]);
-        message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
+        return message.channel.send(`secsses the role ${role} has been set for ${args[2]} coins`)
       }
       if(args[0] >= "11") return message.channel.send("only beateen 1 - 10");
     }
