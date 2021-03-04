1a01f539287da09def4bfeb331a234b877eba019
Glitch (honey-lava-orca) <none>
2021-03-04 03:45:33 +0000

    🍳🏯 Checkpoint ./commands/prison.js:29068751/14

--- commands/prison.js
+++ commands/prison.js
@@ -9,7 +9,7 @@ module.exports = {
     async run (client, message, args) {
   let staffrole = message.guild.roles.cache.find(x => x.id === "814599275446927408")
   
-  if (message.member.roles.cache.some(staffrole)){
+  if (message.member.roles.find(staffrole)){
         let presonmem = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
       if (presonmem === message.author) return message.channel.send("you cant jail your self")
       let logs = message.guild.channels.cache.find(x => x.id === "816788429656293446")
