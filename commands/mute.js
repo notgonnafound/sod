8af63cadce3cb7bd99adada452f4539420370ccf
Glitch (honey-lava-orca) <none>
2021-03-06 02:46:08 +0000

    📷👚 Checkpoint ./commands/mute.js:29068751/1297

--- commands/mute.js
+++ commands/mute.js
@@ -12,11 +12,21 @@ module.exports = {
     if (prefix === null) prefix = default_prefix;
       
       let muterole = db.fetch(`muterole_${message.guild.id}`)
-      if (!muterole) return message.channel.send(`pls give me the id of the muted role by \n ${default_prefix}mute roleset [role-id]`)
+      if (!muterole) return message.channel.send(`pls give me the id of the muted role by \n${prefix}mute roleset [role-id]`)
+      
+      if (args[0].toLowercase === "rpleset"){
+         if(!message.member.hasPermission("ADMINISTRATOR")) return message.channel.send('you dont have permission to do that for administrator only');
+         if(!args[1]) return message.channel.send(`pls give me the role id by using ${prefix}mute roleset [role-id]`)
+         if(!message.guild.roles.cache.find(x => x.id === args[1])) return message.channel.send("i dident find that role pls try again")
+        db.set(`muterole_${message.guild.id}`, args[1])
+        return message.channel.send("")
+         
+      }
+      
       
       let tomute = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
       if(!tomute) return message.channel.send("pls mention a user pls");
-      let role = message.guild.roles.cache.find(x => x.id === "816267328869892137");
+      let role = message.guild.roles.cache.find(x => x.id === db.fetch(`muterole_${message.guild.id}`));
       let time = args[1];
       if(!time) return message.channel.send("how mutch time thr mute is gonna be?");
       let reason = args.slice(2).join(" ");
